3e446727c23098b1f02500d0d6ef3a298e762026
Wolf <eoinospealain@gmail.com>
2020-04-30 14:13:31 +0100

    Update wolf.py

--- src/wolf.py
+++ src/wolf.py
@@ -1,24 +1,25 @@
+__author__ = "Wolf Ó Spealáin"
+__date__ = "April 2020"
+__credits__ = "Part of Skills Demo 3"
+__version__ = 0.1
+
+
 def colour():
     """
-    Prompts the user to pick a colour from a menu and returns the colour name.
+    Prompts the user to pick a colour from a menu and returns the RGB code.
     Wolf, April 2020.
     """
-    print(""
-          "1) Black"
-          "2) White"
-          "3) Red"
-          "3) Green"
-          "4) Blue")
+    print("\nCOLOUR MENU\n\n1) Black\n2) White\n3) Red\n3) Green\n4) Blue\n")
     selection = -1
-    while selection not in range(0,5): 
+    while selection not in range(0, 5):
         selection = input("Please select a colour (0-4): ")
-        if colour == 0:
+        if selection == "0":
             return "#000000"
-        elif colour == 1:
+        elif selection == "1":
             return "#FFFFFF"
-        elif colour == 2:
+        elif selection == "2":
             return "#FF0000"
-        elif colour == 3:
+        elif selection == "3":
             return "#00FF00"
-        elif colour == 4:
+        elif selection == "4":
             return "#0000FF"
