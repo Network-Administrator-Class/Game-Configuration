9d11dfa0b88b4d8f2764d5bac1fa7f8b919b5590
Wolf <eoinospealain@gmail.com>
2020-04-30 16:57:01 +0100

    Update wolf.py

--- src/wolf.py
+++ src/wolf.py
@@ -1,3 +1,5 @@
+"""Sample Code"""
+
 __author__ = "Wolf Ó Spealáin"
 __date__ = "April 2020"
 __credits__ = "Part of Skills Demo 3"
@@ -11,7 +13,7 @@ def colour():
     """
     print("\nCOLOUR MENU\n\n1) Black\n2) White\n3) Red\n3) Green\n4) Blue\n")
     selection = -1
-    while selection not in range(0, 5):
+    while selection not in ["0", "1", "2", "3", "4"]:
         selection = input("Please select a colour (0-4): ")
         if selection == "0":
             return "#000000"
@@ -23,3 +25,15 @@ def colour():
             return "#00FF00"
         elif selection == "4":
             return "#0000FF"
+
+
+def buy_weapon():
+    """Allows user to buy a weapon with money."""
+    selection = None
+    print("\nWeapons:\n1) Sword (150)\n2) Stick (5)\n0) Leave with buying.")
+    while selection not in ["0", "1", "2"]:
+        selection = input("Pick a weapon (1-2): ")
+        if selection == "1":
+            return "Sword", 150
+        elif selection == "2":
+            return "Stick", 5
