f07396f01b4a0398858cdd72f1976d10af86134d
FearghalH <fearghalhayes2019@mallowcollege.email>
2020-05-20 19:16:43 +0100

    Added documentation to my fearghal.py and fearghals_functions.py

--- fearghal.py
+++ fearghal.py
@@ -1,5 +1,8 @@
 # !/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+This module contains the options menu for the program.
+"""
 from __future__ import print_function
 import brianr
 import Ben_audio
@@ -7,8 +10,16 @@ import kolaczkowski
 import craig
 import ade
 
+__author__ = "Fearghal Hayes"
+__date__ = "03/05/2020"
+__credits__ = "Me, and the other participants in the Skills Demo 3 for the Programming Module"
+__version__ = "0.0.1"
+
 
 def options():
+    """
+    This is the options menu for the program, and runs when you press "5" on the main menu
+    """
     print('\n\tOptions Menu \n\n'
           '1) Audio\n'
           '2) Gameplay\n'
@@ -20,28 +31,33 @@ def options():
     while option not in ["1", "2", "3", "4", "5"]:
         option = input("Please select a Main Menu option between 1 and 5: ")
         if option == "1":
-            the_audio = Ben_audio.audio_options()
+            the_audio = Ben_audio.audio_options()  # This is Bento's function integrated into my options menu.
             return the_audio
 
         elif option == "2":
-            the_gameplay = craig.gameplay()
+            the_gameplay = craig.gameplay()  # This is Craigs's function integrated into my options menu.
             return the_gameplay
 
         elif option == "3":
-            the_display_menu = display()
+            the_display_menu = display()  # This is my "display" function integrated into my options menu.
             return the_display_menu
 
         elif option == "4":
             return {}
-        elif option == "5":
+        elif option == "5":  # If the user choosing nothing, return an empty dictionary.
             print("Goodbye")
             return {}
 
         else:
-            print("Invalid option, please select an option from the list.")
+            print("Invalid option, please select an option from the list.")  # The user must input a valid number,
+            # otherwise the program will continue to loop until one has been entered.
 
 
 def display():
+    """
+    Fearghal's display menu function, which is launched when the usr picks option "3" in the options menu
+    """
+
     print('\n\tDisplay Menu \n\n'
           '1) Resolution\n'
           '2) Video Mode\n'
@@ -50,29 +66,31 @@ def display():
           '5) Quality\n'
           '6) Exit\n')
 
-    # print("\nResolution \n1) Video Mode\t\t2) Frame Rate Limit\n3) Vertical Sync\t\t4) Quality\t\t5)")
     the_display = 0
     while the_display not in ["1", "2", "3", "4", "5", "6"]:
         the_display = input("Please select a Main Menu option between 1 and 5: ")
         if the_display == "1":
-            resolution = brianr.screen_resolution()
+            resolution = brianr.screen_resolution()  # Brian's screen resolution function is called when "1" is entered.
             return resolution
         elif the_display == "2":
-            the_video_mode = ade.video_mode()
+            the_video_mode = ade.video_mode()  # Ade's video mode function is called when "2" is entered.
             return the_video_mode
         elif the_display == "3":
-            the_fps_limit = kolaczkowski.framerate_limit()
+            the_fps_limit = kolaczkowski.framerate_limit()  # Tomasz's framerate limit function is called when "3" is
+            # entered.
             return the_fps_limit
         elif the_display == "4":
             return {}
         elif the_display == "5":
             return {}
-        elif the_display == "6":
+        elif the_display == "6":  # If the user exits, then no data is entered, so we must return an empty dictionary.
             print("Goodbye")
             return {}
         else:
-            print("Invalid option, please select an option from the list.")
+            print("Invalid option, please select an option from the list.")  # The user must input a valid number,
+            # or else the loop will keep on looping
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # This prevents the user from running my function directly from the command line or
+    # otherwise and instead tells the user to run the "setup.py" file instead
     print("HEY! Don't run me directly, I'm a module! I serve as a library for the setup.py file, run that instead.")
--- fearghals_functions.py
+++ fearghals_functions.py
@@ -26,13 +26,16 @@ def add_settings_to_start(passed_settings={}, passed_filename="config.json"):  #
 
 
 def load(passed_filename="config.json", debug_level=None):  # CALL ME WHEN NEEDED!
-    """"""
+    """This function is for loading the data from the file. Usually, you would store this functions value to a variable,
+    then call the variable to get information from the file."""
     settings = backend_load(passed_filename, debug_level)
     return settings
 
 
 def show(filename="config.json"):  # CALL ME WHEN NEEDED!
-    """"""
+    """This function is for showing the user the current JSON data in the file. You normally wouldn't assign a
+    variable to this function, and would instead call this function directly with the file you want to show the data
+    of as a parameter."""
     the_data = backend_load(filename)
 
     if the_data != {}:
@@ -48,17 +51,16 @@ def initial_save(passed_settings, passed_filename="config.json"):
 
 def backend_load(filename="config.json", debug_level=None):  # DON'T CALL ME DIRECTLY!
     """Used for loading the filename, do not call this function, instead call the \"append_settings\" function,
-    or the \"add_settings_to_start\" for either task. This function and the \"backend_save\" function are the backbone for
-    loading and saving file (But don't use them directly! I have wrapper functions setup for you to use,
-    use them instead!)"""
+    or the \"add_settings_to_start\" for either task. This function and the \"backend_save\" function are the
+    backbone for loading and saving file (But don't use them directly! I have wrapper functions setup for you to use,
+    use them instead!) """
 
     if isfile(filename):
         with open(filename) as target:
             try:
                 json_data = json.load(target)
                 if debug_level == 1:
-                    print("DEBUGGING: The current json data is:", json_data)
-
+                    print("DEBUGGING: The current json data is:", json_data)  # This is for debugging purposes.
                 return json_data
 
             except json.JSONDecodeError:
@@ -71,7 +73,9 @@ def backend_load(filename="config.json", debug_level=None):  # DON'T CALL ME DIR
 
 
 def backend_save(answers={}, filename="config.json"):  # DON'T CALL ME DIRECTLY!
-    """"""
+    """This function saves the data passed into it to the file passed into it. Don't call this file directly,
+    instead use the wrapper functions at the top of this file, the "append_settings", or the "add_settings_to_start"
+    functions, for either of your needs."""
 
     with open(filename, "w") as target:
         output = json.dump(answers, target, indent=4, sort_keys=True)
@@ -79,7 +83,9 @@ def backend_save(answers={}, filename="config.json"):  # DON'T CALL ME DIRECTLY!
 
 
 def backend_append_values(answers={}, filename="config.json"):  # DON'T CALL ME DIRECTLY!
-    """"""
+    """This function is required by the "append_settings" function to append the data passed into it into the passed
+    filename. Don't call this file directly, instead use the wrapper function at the top of this file,
+    the "append_settings" function."""
 
     the_data = backend_load(filename)
     the_data.update(answers)
@@ -87,7 +93,9 @@ def backend_append_values(answers={}, filename="config.json"):  # DON'T CALL ME 
 
 
 def backend_add_values_to_start(answers={}, filename="config.json"):  # DON'T CALL ME DIRECTLY!
-    """"""
+    """This function does the same this as the above function "backend_append_values", but adds the data to the start
+    of the file. Don't call this file directly, instead use the wrapper function at the top of this file,
+    the "add_settings_to_start" function."""
 
     the_data = backend_load(filename)
     answers.update(the_data)
